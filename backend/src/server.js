d84de5543475d2efeb45d5f7e8c572428359f526
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-04-13 21:07:28 +0530

    Minor Update

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const { app } = require("./app.js");
 const { env } = require("./config");
 
-const PORT = env.PORT;
+const PORT = env.PORT || 5007;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);                                                                                                                                                
